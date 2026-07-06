a9365f138ba7dadf2f97f24dbbb740acc9978e09
noelkcris-alt <noelkcris@gmail.com>
2026-07-06 19:49:12 +0800

    Update master_gateway0.js

--- master_gateway0.js
+++ master_gateway0.js
@@ -1,13 +1,9 @@
 // REPLACE THESE WITH YOUR EXACT SUPABASE CREDENTIALS FROM YOUR SETTINGS PANEL
 const SUPABASE_URL = "https://tsalfqpvcxyauhhnonog.supabase.co";
 const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRzYWxmcXB2Y3h5YXVoaG5vbm9nIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODI5MTc4MDcsImV4cCI6MjA5ODQ5MzgwN30.STlqr1DZcabM6wd0nJZc40QsMUjojqW-2a-yBYWZqqg";
-
-
 // =====================================================================
 // FRONT-END GLOBAL CONFIGURATIONS & API ACCESS CHANNELS
 // =====================================================================
-
-
 const apiHeaders = {
     'apikey': SUPABASE_ANON_KEY,
     'Authorization': `Bearer ${SUPABASE_ANON_KEY}`,
